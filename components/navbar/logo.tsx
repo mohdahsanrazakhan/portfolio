13b5a6ed2c287a75ea0749070366ce54d3a710a8
md_mark <datawithmark@gmail.com>
2026-08-07 00:50:46 +0530

    fix: added / in logo

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import { bytesizedFont } from "@/app/layout";
 
 export const Logo = () => (
-  <div className="flex items-center gap-2">
+  <Link href="/" className="flex items-center gap-2">
     <p className={`${bytesizedFont.className} text-3xl`}>MARK</p>
-  </div>
+  </Link>
 );
